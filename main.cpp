22d3b30ee7c7cc37a89bafc1719415b1207c0f92
lwidm <lukas@widmer.id.au>
2026-02-19 12:47:17 -0800

    chapter 0 done

--- main.cpp
+++ main.cpp
@@ -1,29 +1,48 @@
 #include <vulkan/vulkan_raii.hpp>
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
     void run() {
-        initVulkan();
-        mainLoop();
-        cleanup();
+        this->initWindow();
+        this->initVulkan();
+        this->mainLoop();
+        this->cleanup();
     }
 
 private:
+    GLFWwindow* window = nullptr;
+
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        this->window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(this->window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
     }
 };
 
