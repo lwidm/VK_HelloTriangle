ec6719009cce41836ff019850b4b2084800c2d40
lwidm <lukas@widmer.id.au>
2026-02-19 13:31:38 -0800

    another chapter done

--- main.cpp
+++ main.cpp
@@ -1,27 +1,36 @@
+#include "vulkan/vulkan.hpp"
+#include <algorithm>
+#include <cstdint>
+#include <vulkan/vulkan_core.h>
 #include <vulkan/vulkan_raii.hpp>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
-#include <cstdlib>
 
-constexpr uint32_t WIDTH = 800;
+constexpr uint32_t WIDTH  = 800;
 constexpr uint32_t HEIGHT = 600;
 
-class HelloTriangleApplication {
-public:
-    void run() {
+class HelloTriangleApplication
+{
+  public:
+    void run()
+    {
         this->initWindow();
         this->initVulkan();
         this->mainLoop();
         this->cleanup();
     }
 
-private:
-    GLFWwindow* window = nullptr;
+  private:
+    GLFWwindow        *window = nullptr;
+    vk::raii::Context  context;
+    vk::raii::Instance instance = nullptr;
 
-    void initWindow() {
+    void initWindow()
+    {
         glfwInit();
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
@@ -29,29 +38,66 @@ private:
         this->window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
-    void initVulkan() {
-
+    void initVulkan()
+    {
+        createInstance();
     }
 
-    void mainLoop() {
-        while (!glfwWindowShouldClose(this->window)) {
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(this->window))
+        {
             glfwPollEvents();
         }
     }
 
-    void cleanup() {
+    void cleanup()
+    {
         glfwDestroyWindow(window);
 
         glfwTerminate();
     }
+
+    void createInstance()
+    {
+        constexpr vk::ApplicationInfo appInfo{
+            .pApplicationName   = "Hello Triangle",
+            .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+            .pEngineName        = "No Engine",
+            .apiVersion         = vk::ApiVersion14};
+
+        uint32_t glfwExtensionCount = 0;
+        auto     glfwExtensions     = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+        {
+            if (std::ranges::none_of(extensionProperties,
+                                     [glfwExtension = glfwExtensions[i]](auto const &extensionProperty) { return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+            {
+                throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+            }
+        }
+
+        vk::InstanceCreateInfo createInfo{
+            .pApplicationInfo        = &appInfo,
+            .enabledExtensionCount   = glfwExtensionCount,
+            .ppEnabledExtensionNames = glfwExtensions};
+
+        instance = vk::raii::Instance(context, createInfo);
+    }
 };
 
-int main() {
+int main()
+{
     HelloTriangleApplication app;
 
-    try {
+    try
+    {
         app.run();
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
